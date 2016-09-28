cf36c583e542b8c96088a9231ed725d95a1d2707
alumno <alumno@10.0.2.15>
2016-09-28 09:53:40 +0200

    Nuevo mensaje escrito en pantalla

---
+++ PracticaU0A02/src/PruebaGit.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class PruebaGit {
+	public static void main (String[]args){
+		
+		Scanner sc = new Scanner(System.in);
+		String maquina;
+		
+		System.out.println("Introduce el nombre de la máquina: ");
+		maquina = sc.next();
+		
+		System.out.println("El nombre de la máquina es: "+maquina);
+	}
+}
